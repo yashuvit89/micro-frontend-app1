c28009496b1c1f68d9e37d627e7c0b9b6323c93d
P S L Yaswanth <yashu.vit@gmail.com>
2020-02-01 23:26:13 +0530

    basic integration micro-frontends

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import styled, { keyframes } from "styled-components";
+
+const pulse = keyframes`
+  from {
+    transform: rotate(360deg);
+  }
+  to {
+    transform: rotate(0deg);
+  }
+`;
+
+const AppLogo = styled.img.attrs({
+  src: logo
+})`
+  height: 30vmin;
+  pointer-events: none;
+  animation: ${pulse} infinite 10s linear;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <AppLogo></AppLogo>
         <p>APP 1</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
